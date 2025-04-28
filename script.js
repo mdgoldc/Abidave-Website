cb06d82f8d9343eed8d148883f8bd4ee73a763a1
mdgoldc <122994043+mdgoldc@users.noreply.github.com>
2025-04-28 14:44:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Popup Quote Form
+function openQuoteForm() {
+    document.getElementById("quoteForm").style.display = "flex";
+  }
+  
+  function closeQuoteForm() {
+    document.getElementById("quoteForm").style.display = "none";
+  }
+  
+  // Image Slider
+  let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("active");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex - 1].classList.add("active");
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
+  }
